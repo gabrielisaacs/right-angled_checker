f9751edfda72937a0d65f87e451f3805d2aea257
Gideon Kelechi John <gideonkelechijohn@gmail.com>
2026-03-09 19:55:12 +0100

    Added logic to check if triangle is right-angled

--- program.c
+++ program.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int angle1, angle2, angle3;
+
+    printf("Enter first angle: ");
+    scanf("%d", &angle1);
+
+    printf("Enter second angle: ");
+    scanf("%d", &angle2);
+
+    angle3 = 180 - (angle1 + angle2);
+
+    if(angle1 == 90 || angle2 == 90 || angle3 == 90) {
+        printf("The triangle is right-angled.\n");
+    } else {
+        printf("The triangle is not right-angled.\n");
+    }
+
+    return 0;
 }

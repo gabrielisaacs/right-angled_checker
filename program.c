1504d4f8e386af4fc197bb4e9fad4fd9baca5dd2
Gabriel Isaac <stewarti3@yahoo.com>
2026-03-10 01:25:42 +0100

    Refactor logic to check for invalid angles

--- program.c
+++ program.c
@@ -11,7 +11,9 @@ int main() {
 
     angle3 = 180 - (angle1 + angle2);
 
-    if(angle1 == 90 || angle2 == 90 || angle3 == 90) {
+    if (angle1 <=0 || angle2 <= 0 || angle3 <= 0){
+        printf("The triangle is not valid. \nYou can't have a triangle with an angle less than or equal to 0 (zero).\n");
+    } else if(angle1 == 90 || angle2 == 90 || angle3 == 90) {
         printf("The triangle is right-angled.\n");
     } else {
         printf("The triangle is not right-angled.\n");
